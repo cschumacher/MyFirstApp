a092ddd1170818456d5f3b71250c0bd31eefaedf
cschumacher <schumacr@mail.uc.edu>
2013-05-15 21:52:06 -0400

    This might be broken, Watch out yo.

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * @author cschumacher
+ * {@literal} Date 05-12-13
+ */
 
 public class MainActivity extends Activity {
-
+public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,18 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    /**
+     * Called when the user clicks the Send button 
+     * @param view
+     */
+    public void sendMessage(View view){
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
     
 }
